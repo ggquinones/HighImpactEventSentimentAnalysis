e5bd178699806ec5712d84d68663aad26a51188b
Gabriel Quinones <quingg01@students.ipfw.edu>
2018-10-01 11:29:08 -0400

    Initial Commit

---
+++ DataProcessing/processTweets.py
@@ -0,0 +1,24 @@
+import csv
+import nltk
+#from nltk.corpus import stopwords 
+from nltk.tokenize import word_tokenize 
+nltk.download('punkt')
+
+stop_words = ['ourselves', 'hers', 'between', 'yourself', 'but', 'again', 'there', 'about', 'once', 'during', 'out', 'very', 'having', 'with', 'they', 'own', 'an', 'be', 'some', 'for', 'do', 'its', 'yours', 'such', 'into', 'of', 'most', 'itself', 'other', 'off', 'is', 's', 'am', 'or', 'who', 'as', 'from', 'him', 'each', 'the', 'themselves', 'until', 'below', 'are', 'we', 'these', 'your', 'his', 'through', 'don', 'nor', 'me', 'were', 'her', 'more', 'himself', 'this', 'down', 'should', 'our', 'their', 'while', 'above', 'both', 'up', 'to', 'ours', 'had', 'she', 'all', 'no', 'when', 'at', 'any', 'before', 'them', 'same', 'and', 'been', 'have', 'in', 'will', 'on', 'does', 'yourselves', 'then', 'that', 'because', 'what', 'over', 'why', 'so', 'can', 'did', 'not', 'now', 'under', 'he', 'you', 'herself', 'has', 'just', 'where', 'too', 'only', 'myself', 'which', 'those', 'i', 'after', 'few', 'whom', 't', 'being', 'if', 'theirs', 'my', 'against', 'a', 'by', 'doing', 'it', 'how', 'further', 'was', 'here', 'than'] 
+
+
+
+with open('parklandShooting.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=';')
+    line_count = 0
+    for row in csv_reader:
+    	try:
+    		# row[4] is the actual tweet
+    		word_tokens = word_tokenize(row[4])
+    		filtered_sentence = [w for w in word_tokens if not w in stop_words]
+    		filtered_sentence = [w for w in filtered_sentence if "/" not in w ]
+    		filtered_sentence = [w for w in filtered_sentence if "\\" not in w ]
+    		print(filtered_sentence)
+    		print("-----------")
+    	except:
+    		continue
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # HighImpactEventSentimentAnalysis
+### Events Being Analyzed
+1. July 20, 2012-Aurora theater shooting
+2. December 12, 2012-Sandy Hook
+3. September 16, 2013-Washington Navy Yard shooting
+4. October 1, 2015-Umpqua Community College shooting
+5. November 27,2015-Colorado Springs Planned Parenthood shooting
+6. December 2, 2015-San Bernardino attack
+7. June 17, 2015-Charleston church shooting
+8. June 12, 2016-Orlando nightclub shooting
+9. July 7, 2016-Dallas Police officers shooting
+10. January 6, 2017-Fort Lauderdale airport shooting
+11. April 10, 2017-North Park Elementary School shooting
+12. October 1, 2017-Las Vegas shooting
+13. November 5, 2017-Sutherland Springs church shooting
+14. December 31, 2017-Copper Canyon Apartment shooting
+15. February 14, 2018-Stoneman Douglas(Parkland Florida) High School shooting
+16. May 18, 2018-Santa Fe High School shooting
