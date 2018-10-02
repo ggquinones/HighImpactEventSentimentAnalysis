85a5a914cb6ac7115ea7da04ebb40cc82ca05d7d
Gabriel Quinones <quingg01@students.ipfw.edu>
2018-10-01 22:50:31 -0400

    Data Processing scripts

--- DataProcessing/processTweets.py
+++ DataProcessing/processTweets.py
@@ -1,4 +1,5 @@
 import csv
+import preprocessor as p
 import nltk
 from nltk.corpus import stopwords 
 from nltk.tokenize import word_tokenize 
@@ -6,20 +7,31 @@ from nltk.tokenize import word_tokenize
 nltk.download('stopwords')
 nltk.download('punkt')
 stop_words = set(stopwords.words('english'))
+punctuation = [',','.','(',')','//',':',';','<','>','?','!','@']
 
 
 
-with open('parklandShooting.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=';')
-    line_count = 0
-    for row in csv_reader:
-    	try:
-			tweet = row[4]
-			word_tokens = word_tokenize(row[4])
-			filtered_sentence = [w for w in word_tokens if "/" not in w]
-			filtered_sentence = [w for w in filtered_sentence if "\\x" not in w]		
-			filtered_sentence = [w for w in filtered_sentence if  w not in  stop_words]		
-			print(filtered_sentence)
-			print("-----------")
-    	except:
-    		continue
+with open('qorlandonightclubshooting.csv') as csv_file:
+	with open('processedOrlandoShooting.csv', mode='w') as inFile:
+		fw = csv.writer(inFile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+		csv_reader = csv.reader(csv_file, delimiter=';')
+		line_count = 0
+		for row in csv_reader:
+			try:
+				tweet = p.clean(row[4])			
+				word_tokens = word_tokenize(tweet)			
+				filtered_sentence = [w for w in word_tokens if w not in stop_words]
+				filtered_sentence = [w for w in filtered_sentence if "/" not in w]
+				filtered_sentence = [w for w in filtered_sentence if "\\" not in w]
+				filtered_sentence = [w for w in filtered_sentence if w not in punctuation]	
+				ans = []
+				for tk in word_tokens:
+					if "http" in tk or "https" in tk:
+						break
+					ans.append(tk)
+						
+				fw.writerow(ans)
+				print("Tweet processed...")
+			except:
+				continue
+
--- DataProcessing/test.py
+++ DataProcessing/test.py
@@ -1,35 +1,12 @@
+import twint
 
-import pandas as pd
-import nltk
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize, sent_tokenize
+# Configure
+c = twint.Config()
+c.Search = "aurora shooting"
+c.Since = "2012-07-20"
+c.Until = "2012-07-28"
+c.Output = "test.csv"
+c.Format = "{tweet}"
 
-#---------Download list of stopwords----------
-nltk.download('stopwords')
-nltk.download('punkt')
-
-#---------Set Language to english----------
-stopWords = set(stopwords.words('english')) 
-
-#---------Read the data as a pd dataframe and extract only the text column----------
-data = pd.read_csv("parklandShooting.csv")
-print(data)
-tweets = data[['text']]
-
-#---------Stringify the tweets----------
-tokenData = word_tokenize(str(tweets))
-
-tweet_tokens = [] 
-
-#---------Iterate through the tweets to remove stopwords and '/' characters----------
-tweet_tokens = [w for w in tokenData if not w in stopWords]
-#tweet_tokens = [w for w in tweet_tokens if "/" not in w ]
-#tweet_tokens = [w for w in tweet_tokens if "\\" not in w ]
-
-print(tweet_tokens)
-
-#---------Export the tokenized words as a csv----------
-#result = pd.DataFrame(tweet_tokens)
-#result.to_csv("Sutherland_tokenized.csv", index=False)
-
-
+# Run
+twint.run.Search(c)
