0e1b826c02cf29101d9aa98404e6c9a534fdabef
Gabriel Quinones <quingg01@students.ipfw.edu>
2018-10-03 21:37:08 -0400

    processed tweets,data to own dir

--- DataProcessing/processTweets.py
+++ DataProcessing/processTweets.py
@@ -1,6 +1,7 @@
 import csv
 import preprocessor as p
 import nltk
+import os,csv
 from nltk.corpus import stopwords 
 from nltk.tokenize import word_tokenize 
 
@@ -10,28 +11,28 @@ stop_words = set(stopwords.words('english'))
 punctuation = [',','.','(',')','//',':',';','<','>','?','!','@']
 
 
-
-with open('qorlandonightclubshooting.csv') as csv_file:
-	with open('processedOrlandoShooting.csv', mode='w') as inFile:
-		fw = csv.writer(inFile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-		csv_reader = csv.reader(csv_file, delimiter=';')
-		line_count = 0
-		for row in csv_reader:
-			try:
-				tweet = p.clean(row[4])			
-				word_tokens = word_tokenize(tweet)			
-				filtered_sentence = [w for w in word_tokens if w not in stop_words]
-				filtered_sentence = [w for w in filtered_sentence if "/" not in w]
-				filtered_sentence = [w for w in filtered_sentence if "\\" not in w]
-				filtered_sentence = [w for w in filtered_sentence if w not in punctuation]	
-				ans = []
-				for tk in word_tokens:
-					if "http" in tk or "https" in tk:
-						break
-					ans.append(tk)
-						
-				fw.writerow(ans)
-				print("Tweet processed...")
-			except:
-				continue
+for filename in os.listdir("tweets"):
+	with open('tweets/'+filename) as csv_file:
+		with open('processedTweets/'+filename, mode='w') as inFile:
+			fw = csv.writer(inFile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+			csv_reader = csv.reader(csv_file, delimiter=';')
+			line_count = 0
+			for row in csv_reader:
+				try:
+					tweet = p.clean(row[4])			
+					word_tokens = word_tokenize(tweet)			
+					filtered_sentence = [w.strip() for w in word_tokens if w.strip() not in stop_words]
+					filtered_sentence = [w for w in filtered_sentence if "/" not in w]
+					filtered_sentence = [w for w in filtered_sentence if "\\" not in w]
+					filtered_sentence = [w for w in filtered_sentence if w not in punctuation]	
+					ans = []
+					for tk in word_tokens:
+						if "http" in tk or "https" in tk:
+							break
+						ans.append(tk)
+							
+					fw.writerow(ans)
+					print("Tweet processed...")
+				except:
+					continue
 
